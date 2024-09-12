dedadd98b2f9670d5702b166d170b96b4cca2bb1
Ibaigz <ibaigm04@gmail.com>
2024-09-12 12:50:56 +0200

    logs solucionados

--- plano1.php
+++ plano1.php
@@ -17,27 +17,9 @@ $stmt = $con->prepare($sql);
 $stmt->execute();
 $todos = $stmt->fetchAll();
 
-// Seleccionar las últimas 10 acciones
-$sql = "SELECT * FROM acciones ORDER BY actionID DESC LIMIT 10";
-$stmt = $con->prepare($sql);
-$stmt->execute();
-$log = $stmt->fetchAll();
 
-//Hacer el log más legible
-foreach ($log as $key => $logs) {
-  $sql = "SELECT nombre FROM users WHERE usuarioID = :uid";
-  $stmt = $con->prepare($sql);
-  $stmt->bindParam(':uid', $logs['usuarioID']);
-  $stmt->execute();
-  $uname = $stmt->fetch();
-  $log[$key]['usuarioID'] = $uname['nombre'];
-  $sql2 = "SELECT tipo, piso FROM dispositivo WHERE dispositivoID = :dispositivoID";
-  $stmt2 = $con->prepare($sql2);
-  $stmt2->bindParam(':dispositivoID', $logs['dispositivoID']);
-  $stmt2->execute();
-  $dispositivo = $stmt2->fetch();
-  $log[$key]['dispositivoID'] = $dispositivo['tipo'] . " piso " . $dispositivo['piso'];
-}
+
+
 ?>
 
 
@@ -137,22 +119,24 @@ foreach ($log as $key => $logs) {
 		miDato.append("valor", action);
 		miDato.append("dispositivoID", 1); // Ajusta según sea necesario
 		miDato.append("uid", <?php echo json_encode($_SESSION['userID']); ?>);
-
+    
 		fetch('procesar.php', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded'
 			},
 			body: miDato.toString()
+      
 			})
 			.then(response => response.text())
 			.then(data => {
 			console.log('Respuesta del servidor:', data);
+      repoblar();
 			})
 			.catch(error => {
 			console.error('Error:', error);
 			});
-		repoblar();
+      
 	});
 	// Fin del código de la bombilla
 
